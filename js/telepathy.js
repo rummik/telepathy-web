f92fc7e5875dea6bd2f3c24b6c8b0ca056db54f1
rummik <r@9k1.us>
2013-09-29 20:02:56 -0400

    Allow scrolling on iOS when we're editing settings

--- js/telepathy.js
+++ js/telepathy.js
@@ -44,6 +44,9 @@
 	// Disable scroll if iOS standalone is happening
 	if (window.navigator.standalone) {
 		$(document).on('touchmove', function(event) {
+			if ($('#settings').hasClass('open'))
+				return;
+
 			event.preventDefault();
 			return false;
 		});
